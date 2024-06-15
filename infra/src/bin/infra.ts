08b6fcdf7b19b1ab83ac1e20a72a76a58108ef30
Collin Giess <68926012+GiessC@users.noreply.github.com>
2024-06-15 15:06:57 -0400

    Infra naming changes, AwsSettings are nullable

--- infra/src/bin/infra.ts
+++ infra/src/bin/infra.ts
@@ -8,17 +8,17 @@ import ProductionStack from '../lib/stacks/env/production/productionStack';
 const app = new App();
 const environment = app.node.tryGetContext('environment');
 
-// Use PascalCase for stack IDs. Let AWS pick names for all resources.
-// https://docs.aws.amazon.com/prescriptive-guidance/latest/best-practices-cdk-typescript-iac/best-practices.html
+console.info('Deploying to environment:', environment);
+
 if (!environment || environment === 'development') {
-    new DevelopmentStack(app, 'Ctrl-Dev');
+    new DevelopmentStack(app, 'CtrlDev');
 } else if (environment === 'production') {
-    new ProductionStack(app, 'Ctrl-Prod');
+    new ProductionStack(app, 'CtrlProd');
 } else {
     console.info(
-        `Using local environment. CloudFormation stacks will be created using ID=Ctrl-${environment}`,
+        `Using local environment. CloudFormation stacks will be created using ID=Ctrl${environment}`,
     );
-    new LocalStack(app, `Ctrl-${environment}`);
+    new LocalStack(app, `Ctrl${environment}`);
 }
 
 app.synth();
